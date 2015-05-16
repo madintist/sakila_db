3c2029d52b8dccd4a349c6721a9f05eec482afeb
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-05-16 09:19:26 -0500

    Added template HTML for navigation.

--- index.php
+++ index.php
@@ -30,6 +30,21 @@ $films = $results->fetchAll(PDO::FETCH_ASSOC);
 
   <h2>Films by Title</h2>
 
+  <nav>
+    <ul id="nav">
+      <li class="navItem"><a href="index.php?pg=1" class="link">1</a></li>
+      <li class="navItem"><a href="index.php?pg=2" class="link">2</a></li>
+      <li class="navItem"><a href="index.php?pg=3" class="link">3</a></li>
+      <li class="navItem"><a href="index.php?pg=4" class="link">4</a></li>
+      <li class="navItem"><a href="index.php?pg=5" class="link">5</a></li>
+      <li class="navItem"><a href="index.php?pg=6" class="link">6</a></li>
+      <li class="navItem"><a href="index.php?pg=7" class="link">7</a></li>
+      <li class="navItem"><a href="index.php?pg=8" class="link">8</a></li>
+      <li class="navItem"><a href="index.php?pg=9" class="link">9</a></li>
+      <li class="navItem"><a href="index.php?pg=10" class="link">10</a></li>
+    </ul>
+  </nav>
+
   <ol>
   <?php
     foreach ($films as $film) {
